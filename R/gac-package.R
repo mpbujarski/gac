7aea55cd89318e210dc2316d82f1aac6aae076de
Marcin Bujarski <bujarskim@student.mini.pw.edu.pl>
2014-03-10 17:24:57 +0100

    gac-package

---
+++ R/gac-package.R
@@ -0,0 +1,13 @@
+#' @title Pakiet Do Liczenia Kwadratow I Nie Tylko
+#' 
+#' @description
+#' Pakiet liczy kwadraty roznych liczb i wektorow w niesamowicie ciekawy sposob
+#' 
+#' @details
+#' Korzystamy z wbudowanej R-owej funkcji "^"
+#' 
+#' @name gac-package
+#' @rdname gac-package
+#' @docType package
+#' @author Michal Gacuta and Marcin Bujarski
+invisible(NULL)
