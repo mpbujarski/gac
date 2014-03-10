f4cfb6f19c0f1d7366a1d97dae1b9c459f66825e
Michal Gacuta <gacutam@student.mini.pw.edu.pl>
2014-03-10 17:17:33 +0100

    a

--- r/Michal.R
+++ r/Michal.R
@@ -1,3 +1,12 @@
+#' @title Ta Funkcja Jest Leniwa I Nic Nie Robi
+#' @description 
+#' tyrym tyrym 
+#' @details
+#' brak szczegolow- to tajna funkcja
+#' @return
+#' funkcja zwraca
+#' @examples
+#' Michal(17)
 #' @export
 Michal <- function(x) 
 {
